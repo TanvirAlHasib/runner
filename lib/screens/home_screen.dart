60d28e49c2fa0c127b289f18394393fc969677c5
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-14 15:19:41 +0600

    app bar design done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:runner/constraints/color_constraints.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,28 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(ColorConstraints.splashBackground_0),
+      appBar: myAppBar(context),
+    );
+  }
 
+  // app bar code
+  AppBar myAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Color(ColorConstraints.splashBackground_0),
+      elevation: 3,
+      title: Text("Runner", style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+        color: Color(ColorConstraints.buttonColor),
+        fontWeight: FontWeight.w800
+      ),),
+      automaticallyImplyLeading: false,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.notifications_none, color: Color(ColorConstraints.buttonColor)),
+        )
+      ],
+      actionsPadding: const EdgeInsets.only(right: 8),
     );
   }
 }
