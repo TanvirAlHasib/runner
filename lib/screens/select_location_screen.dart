21a09fa64d48310c89730c8000a378d23bb70dd9
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-09-21 17:03:06 +0600

    app bar done

--- lib/screens/select_location_screen.dart
+++ lib/screens/select_location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../constraints/color_constraints.dart';
 
 class SelectLocationScreen extends StatefulWidget {
   const SelectLocationScreen({super.key});
@@ -10,6 +11,32 @@ class SelectLocationScreen extends StatefulWidget {
 class _SelectLocationScreenState extends State<SelectLocationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: myAppBar(context),
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: const EdgeInsets.only(top: 13, left: 12, right: 12, bottom: 5),
+      ),
+    );
+  }
+
+  // app bar code
+  AppBar myAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Color(ColorConstraints.splashBackground_0),
+      elevation: 3,
+      title: Text("Runner", style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+          color: Color(ColorConstraints.buttonColor),
+          fontWeight: FontWeight.w800
+      ),),
+      automaticallyImplyLeading: false,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.notifications_none, color: Color(ColorConstraints.buttonColor)),
+        )
+      ],
+      actionsPadding: const EdgeInsets.only(right: 8),
+    );
   }
 }
